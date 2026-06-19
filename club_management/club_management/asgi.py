b28eab0524fc08ca32d9b942695e1f7a1ca7f48f
veer <veerprrajput@gmail.com>
2026-06-19 20:59:37 +0530

    fix settin module

--- club_management/club_management/asgi.py
+++ club_management/club_management/asgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'club_management.club_management.settings'
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_management.settings')
+
 application = get_asgi_application()
--- club_management/club_management/wsgi.py
+++ club_management/club_management/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    'club_management.club_management.settings'
-)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_management.settings')
+
 application = get_wsgi_application()
