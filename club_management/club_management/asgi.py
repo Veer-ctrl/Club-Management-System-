f5a0ae94e62e5f5c887a5ba3ac892916d973b478
veer <veerprrajput@gmail.com>
2026-06-19 20:56:37 +0530

    Fix settings module path

--- club_management/club_management/asgi.py
+++ club_management/club_management/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_management.settings')
-
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'club_management.club_management.settings'
+)
 application = get_asgi_application()
--- club_management/club_management/wsgi.py
+++ club_management/club_management/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'club_management.settings')
-
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'club_management.club_management.settings'
+)
 application = get_wsgi_application()
